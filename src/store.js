91e7e461c9a68795f4f93ab28b01098f812decb1
Nirmalya Ghosh <nirmalya.email@gmail.com>
2019-01-02 23:57:49 +0530

    chore(package): Adds connected-react-router

--- src/store.js
+++ src/store.js
@@ -1,8 +1,8 @@
 import { createStore, applyMiddleware, compose } from 'redux'
-import { routerMiddleware } from 'react-router-redux'
+import { routerMiddleware } from 'connected-react-router'
 import thunk from 'redux-thunk'
 import createHistory from 'history/createBrowserHistory'
-import rootReducer from './reducers'
+import createRootReducer from './reducers'
 
 export const history = createHistory()
 
@@ -18,6 +18,13 @@ if (process.env.NODE_ENV === 'development') {
   }
 }
 
-const composedEnhancers = compose(applyMiddleware(...middleware), ...enhancers)
+const composedEnhancers = compose(
+  applyMiddleware(...middleware),
+  ...enhancers
+)
 
-export default createStore(rootReducer, initialState, composedEnhancers)
+export default createStore(
+  createRootReducer(history),
+  initialState,
+  composedEnhancers
+)
